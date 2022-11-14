3c9e800e8ee3d6ef2c23ebb1aec347a7697b4c1f
nokhyun90 <nokhyun90@gmail.com>
2022-11-14 23:11:04 +0900

    high order function test

--- app/src/test/java/com/nokhyun/samplestructure/ExampleUnitTest.kt
+++ app/src/test/java/com/nokhyun/samplestructure/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.nokhyun.samplestructure
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.nio.file.attribute.GroupPrincipal
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun test(){
+        println({ a ->
+            "a: $a"
+        }, { b ->
+            "bbbb: $b"
+        })
+    }
+
+    private fun println(body: (String) -> String, body2: (String) -> String){
+        println(body("first body"))
+        println(body2("second body"))
     }
 }
