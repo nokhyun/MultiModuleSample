fbbed213147d8dcbd39a65993782ab219f9950e7
nokhyun90 <nokhyun90@gmail.com>
2022-12-07 22:58:41 +0900

    checkbox testing...

--- app/src/main/java/com/nokhyun/samplestructure/ui/activity/UiActivity.kt
+++ app/src/main/java/com/nokhyun/samplestructure/ui/activity/UiActivity.kt
@@ -1,13 +1,37 @@
 package com.nokhyun.samplestructure.ui.activity
 
 import android.os.Bundle
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import androidx.databinding.DataBindingUtil
 import com.nokhyun.samplestructure.R
+import com.nokhyun.samplestructure.databinding.ActivityUiBinding
+import timber.log.Timber
 
 class UiActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityUiBinding
+
+    private val ids by lazy {
+        listOf(
+            binding.layoutCustomRadio.radioButton1.id,
+            binding.layoutCustomRadio.radioButton2.id,
+            binding.layoutCustomRadio.radioButton3.id
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ui)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_ui)
+        binding.layoutCustomRadio.radioGroup.children.zip(ids.asSequence()) { view, id ->
+            (view as RadioButton).id = ids[id]
+        }.also {
+            binding.layoutCustomRadio.radioGroup.check(binding.layoutCustomRadio.radioButton1.id)
+        }
+
+        binding.layoutCustomRadio.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            Timber.e("text: ${findViewById<RadioButton>(checkedId).text} :: checkedId: $checkedId")
+        }
     }
 }
