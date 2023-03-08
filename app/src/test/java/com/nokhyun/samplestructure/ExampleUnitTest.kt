42c2a7f68ccbcd6b7ce118e471596a840ba1987f
nokhyun90 <nokhyun90@gmail.com>
2023-03-08 23:28:42 +0900

    시간정렬 테스트

--- app/src/test/java/com/nokhyun/samplestructure/ExampleUnitTest.kt
+++ app/src/test/java/com/nokhyun/samplestructure/ExampleUnitTest.kt
@@ -4,6 +4,8 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import java.nio.file.attribute.GroupPrincipal
+import java.time.LocalDate
+import java.time.LocalTime
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -18,11 +20,17 @@ class ExampleUnitTest {
 
     @Test
     fun test(){
-        println({ a ->
-            "a: $a"
-        }, { b ->
-            "bbbb: $b"
-        })
+//        println({ a ->
+//            "a: $a"
+//        }, { b ->
+//            "bbbb: $b"
+//        })
+
+        val list = listOf(LocalTime.of(22,22,33), LocalTime.of(22,22,30), LocalTime.of(22,22,38))
+        val sorting = list.sortedByDescending { it }
+
+        println("list: $list")
+        println("sorting: $sorting")
     }
 
     private fun println(body: (String) -> String, body2: (String) -> String){
