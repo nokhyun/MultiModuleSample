9faebe57a2fd8e4c6721b09aaf093dcb4b7529d0
nokhyun90 <nokhyun90@gmail.com>
2023-03-22 22:51:42 +0900

    suspend test

--- app/src/test/java/com/nokhyun/samplestructure/CoroutineTest.kt
+++ app/src/test/java/com/nokhyun/samplestructure/CoroutineTest.kt
@@ -1,16 +1,16 @@
 package com.nokhyun.samplestructure
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.test.*
-import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
+import timber.log.Timber
 
 class MainDispatcherRule(
     val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
@@ -28,11 +28,53 @@ class CoroutineTest {
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun settingMainDisaptcher() = runTest {// Main  scheduler
-        val viewModel = HomeViewModel()
-        viewModel.loadMessage()
-        assertEquals("Greetings!", viewModel.message.value)
+//        val viewModel = HomeViewModel()
+//        viewModel.loadMessage()
+//        assertEquals("Greetings!", viewModel.message.value)
+
+        launch { test2() }
+        test1()
+        /* result
+          test2: 0
+          test2: 1
+          test2: 2
+          test2: 3
+          test2: 4
+          test2: 5
+          test2: 6
+          test2: 7
+          test1: 0
+          test1: 1
+          test1: 2
+          test2: 8
+          test2: 9
+          test1: 3
+          */
+
+//        test2()
+//        test1()
+        /* 절차 */
+    }
+
+    //        private fun test1() {
+    private suspend fun test1() {
+        withContext(Dispatchers.IO) {
+            (0..100).forEach {
+                println("test1: $it")
+            }
+        }
+    }
+
+        private fun test2() {
+//    private suspend fun test2() {
+//        withContext(Dispatchers.IO) {
+            (0..100).forEach {
+                println("test2: $it")
+            }
+//        }
     }
 }
 
