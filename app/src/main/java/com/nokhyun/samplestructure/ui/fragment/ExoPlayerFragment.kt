e2e25e7b0d2b5daa35fb493ac8272abee536b694
nokhyun90 <nokhyun90@gmail.com>
2023-04-03 23:09:13 +0900

    가변 품질 스트리밍

--- app/src/main/java/com/nokhyun/samplestructure/ui/fragment/ExoPlayerFragment.kt
+++ app/src/main/java/com/nokhyun/samplestructure/ui/fragment/ExoPlayerFragment.kt
@@ -7,6 +7,8 @@ import android.view.WindowInsetsController
 import androidx.fragment.app.viewModels
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.util.MimeTypes
 import com.nokhyun.samplestructure.R
 import com.nokhyun.samplestructure.databinding.FragmentExoplayerBinding
 
@@ -24,19 +26,37 @@ class ExoPlayerFragment : BaseFragment<FragmentExoplayerBinding>() {
     override fun setView(view: (layoutId: Int) -> View): View = view(R.layout.fragment_exoplayer)
 
     private fun initializePlayer() {
-        player = ExoPlayer.Builder(requireContext()).build().also {
-            binding.videoView.player = it
+        // DASH(Dynamic Adaptive Streaming over HTTP) : Http 웹 서버에서 제공 되는 인터넷을 통해 미디어 콘텐츠의 고품질 스트리밍을 가능하게 하는 적응형 비트 전송률 스트리밍 기술.
+        // DASH 콘텐츠를 스트리밍하려면 MediaItem 을 만들고, fromUri 가 아닌 MediaItem.Builder 를 사용해야함.
+        //
+        val trackSelector = DefaultTrackSelector(requireContext()).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())    // 표준 화질 이하의 트랙 설정
         }
+
+        player = ExoPlayer.Builder(requireContext())
+            .setTrackSelector(trackSelector)
+            .build()
+            .also {
+                binding.videoView.player = it
+            }
 
 //        val mediaItem = MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/play.mp3")
 //        val mediaItem = MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/Jazz_In_Paris.mp3")
-        val mediaItem = MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+//        val mediaItem = MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+
+        val mediaItem = MediaItem.Builder()
+            .setUri(resources.getString(R.string.media_url_dash))
+            .setMimeType(MimeTypes.APPLICATION_MPD)
+            .build()
         binding.videoView.player?.apply {
             setMediaItem(mediaItem)
             playWhenReady = exoPlayerViewModel.playWhenReady
             seekTo(exoPlayerViewModel.currentWindow, exoPlayerViewModel.playbackPosition)
             prepare()
         }?.also { it.play() }
+
+
+
     }
 
     override fun onResume() {
