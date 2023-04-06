8333ee7e109afcaada5e0a2db6175362c3c1f978
nokhyun90 <nokhyun90@gmail.com>
2023-04-06 23:26:11 +0900

    startup 수정

--- app/src/main/java/com/nokhyun/samplestructure/workmanager/WorkManagerInitializer.kt
+++ app/src/main/java/com/nokhyun/samplestructure/workmanager/WorkManagerInitializer.kt
@@ -1,32 +1,33 @@
 package com.nokhyun.samplestructure.workmanager
 
 import android.content.Context
+import android.util.Log
 import androidx.startup.Initializer
-import androidx.work.Configuration
 import androidx.work.WorkManager
 import timber.log.Timber
 
-//class WorkManagerInitializer : Initializer<WorkManager> {
-//    override fun create(context: Context): WorkManager {
-//        val configuration = Configuration.Builder().build()
-//        WorkManager.initialize(context, configuration)
-//        return WorkManager.getInstance(context)
-//    }
-//
-//    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
-//        return emptyList<Class<out Initializer<*>>>().toMutableList()
-//    }
-//}
+class WorkManagerInitializer : Initializer<WorkManager> {
+    override fun create(context: Context): WorkManager {
+        Log.e(this.javaClass.simpleName, "Work create")
+        return WorkManager.getInstance(context)
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        Log.e(this.javaClass.simpleName, "Work dependencies")
+        return emptyList()
+    }
+}
 
 
 class ExampleLoggerInitializer : Initializer<Unit> {
     override fun create(context: Context) {
+        Log.e(this.javaClass.simpleName, "Exam create")
         return Timber.plant(Timber.DebugTree())
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
+        Log.e(this.javaClass.simpleName, "Exam dependencies")
         // WorkManager 가 초기화 된 후 초기화 됨.
-//        return listOf(WorkManagerInitializer::class.java).toMutableList()
-        return emptyList()
+        return listOf(WorkManagerInitializer::class.java)
     }
 }
