a81ee6dd1860ef599e48c73422dd458f6a033bcc
nokhyun90 <nokhyun90@gmail.com>
2023-08-24 22:39:37 +0900

    - NotificationGroup Sample

--- app/src/main/java/com/nokhyun/samplestructure/ui/fragment/NotificationFragment.kt
+++ app/src/main/java/com/nokhyun/samplestructure/ui/fragment/NotificationFragment.kt
@@ -1,8 +1,8 @@
 package com.nokhyun.samplestructure.ui.fragment
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.app.Service
 import android.content.Intent
 import android.view.View
@@ -13,6 +13,7 @@ import com.nokhyun.samplestructure.ui.activity.MainActivity
 
 class NotificationFragment : BaseFragment<FragmentNotificationBinding>() {
     private lateinit var notificationManager: NotificationManager
+    private var notifyId = 0
 
     override fun init() {
         binding.btnNotification.setOnClickListener { start() }
@@ -28,16 +29,48 @@ class NotificationFragment : BaseFragment<FragmentNotificationBinding>() {
     }
 
     private fun start() {
-        val pi = PendingIntent.getActivity(requireContext(), 1001, Intent(requireContext(), MainActivity::class.java), PendingIntent.FLAG_IMMUTABLE)
+        notificationChannel()
+        notificationGroup()
 
-        val notification = NotificationCompat.Builder(requireContext(), "location")
+        val notification = NotificationCompat.Builder(requireContext(), CHANNEL_ID)
             .setContentTitle("title")
             .setContentText("content")
             .setSmallIcon(R.drawable.ic_launcher_background)
-            .setContentIntent(pi)
-            .setNumber(5)
+            .setGroup(requireContext().packageName)
+            .setGroupSummary(true)
             .setOngoing(true)
 
-        notificationManager.notify(1, notification.build())
+        notificationManager.notify(1234, notification.build())
+    }
+
+    private fun notificationGroup() {
+        val pi = PendingIntent.getActivity(requireContext(), 1001, Intent(requireContext(), MainActivity::class.java), PendingIntent.FLAG_IMMUTABLE)
+
+        val notificationGroup = NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+            .setGroup(requireContext().packageName)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("GroupTitle$notifyId")
+            .setContentTitle("GroupContent$notifyId")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .setBigContentTitle("BigTitle$notifyId")
+                    .setBigContentTitle("BigContents$notifyId")
+            )
+            .setAutoCancel(true)
+            .setContentIntent(pi)
+
+        notificationManager.notify(notifyId++, notificationGroup.build())
+    }
+
+    private fun notificationChannel() {
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val mChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance)
+        mChannel.description = "descriptionText"
+        notificationManager.createNotificationChannel(mChannel)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "NOTIFICATION_ID"
+        private const val CHANNEL_NAME = "NOTIFICATION_NAME"
     }
 }
