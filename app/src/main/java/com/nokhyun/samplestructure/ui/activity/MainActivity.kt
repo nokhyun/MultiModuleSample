656ef858354997a40026c59038b046cd348cc1fd
nokhyun90 <nokhyun90@gmail.com>
2023-09-25 21:54:09 +0900

    - commit test

--- app/src/main/java/com/nokhyun/samplestructure/ui/activity/MainActivity.kt
+++ app/src/main/java/com/nokhyun/samplestructure/ui/activity/MainActivity.kt
@@ -50,8 +50,7 @@ import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.util.*
 import java.util.regex.Pattern
-//2
-// a
+// dev-1
 /**
  * Created by Nokhyun90 on 2022-02-11
  * */
