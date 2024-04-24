403373abf452f776bcd6672e7ea870fbffabffaf
nokhyun90 <nokhyun90@gmail.com>
2024-04-24 23:35:40 +0900

    test: suspendCancellableCoroutine 를 이용한 1회용 버튼

--- app/src/main/java/com/nokhyun/samplestructure/ui/fragment/CoroutineFragment.kt
+++ app/src/main/java/com/nokhyun/samplestructure/ui/fragment/CoroutineFragment.kt
@@ -7,17 +7,29 @@ import com.nokhyun.samplestructure.databinding.FragmentCoroutineBinding
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEmpty
+import kotlinx.coroutines.flow.onErrorReturn
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withTimeoutOrNull
 import timber.log.Timber
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 class CoroutineFragment : BaseFragment<FragmentCoroutineBinding>() {
+    private var job: Job? = null
+
     override fun init() {
         val ceh = CoroutineExceptionHandler { coroutineContext, throwable ->
             Timber.e("throwable: $throwable")
@@ -68,7 +80,7 @@ class CoroutineFragment : BaseFragment<FragmentCoroutineBinding>() {
                     suspendCancellableCoroutine<Unit> { cancellableContinuation ->
                         Timber.e("suspendCancellableCoroutine") // 3초 이후에 취소됨
 
-
+                        cancellableContinuation.resume(Unit)
                         /*
                         * 코루틴의 스코프가 취소 될 때 호출
                         * Job 이 취소되지 않는한 호출되지 않으며, 정상적으로 resume 된다면 해당 블록에 정의된 코드는 동작하지 않음.
@@ -84,7 +96,7 @@ class CoroutineFragment : BaseFragment<FragmentCoroutineBinding>() {
                     }
                 }.let {
                     // 취소가 되므로 동작함.
-                    Timber.e("withTimeoutOrNull result: $it")
+                    Timber.e("suspendCancellableCoroutine withTimeoutOrNull result: $it")
                 }
             }
 
@@ -96,6 +108,50 @@ class CoroutineFragment : BaseFragment<FragmentCoroutineBinding>() {
                     Timber.e("normal  withTimeoutOrNull result: $it")
                 }
             }
+
+            launch {
+                Timber.e("click result: ${click()}")
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            startOngoingMessage(this)
+        }
+
+        binding.btnCancel.setOnClickListener{
+            job?.cancel()
+        }
+    }
+
+    private fun startOngoingMessage(scope: CoroutineScope){
+        job = scope.launch{
+            val ongoingMessageCollect = FlowCollector<String> { Timber.e("ongoingMessage receive: $it") }
+            ongoingMessage()
+                .onStart { Timber.e("ongoingMessage onStart") }
+                .onCompletion { Timber.e("ongoingMessage onCompletion") }
+                .collect(ongoingMessageCollect)
+        }
+    }
+
+    private fun ongoingMessage() = callbackFlow {
+        repeat(10) {
+            delay(1000L)
+            send("message #$it")
+        }
+        awaitClose {
+            Timber.e("ongoingMessage flow close!!!")
+        }
+    }
+
+    /** 1회성 callback 에 사용 */
+    suspend fun click(): String = suspendCancellableCoroutine { continuation ->
+        binding.btnSuspendCoroutine.setOnClickListener {
+            if (!continuation.isCompleted) continuation.resume("Click!!!")
+            startOngoingMessage(viewLifecycleOwner.lifecycleScope)
+        }
+
+        continuation.invokeOnCancellation {
+            continuation.resumeWithException(it ?: Throwable("Click Error"))
         }
     }
 
@@ -104,5 +160,9 @@ class CoroutineFragment : BaseFragment<FragmentCoroutineBinding>() {
 
     override fun setView(view: (layoutId: Int) -> View): View {
         return view(R.layout.fragment_coroutine)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
     }
 }
