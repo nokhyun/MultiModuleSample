d0ef80e27151e3fa0da696843857b9a10d22581f
nokhyun90 <nokhyun90@gmail.com>
2024-05-13 23:19:03 +0900

    Added:Flip Animator

--- app/src/main/java/com/nokhyun/samplestructure/ui/fragment/TransitionFragment.kt
+++ app/src/main/java/com/nokhyun/samplestructure/ui/fragment/TransitionFragment.kt
@@ -1,18 +1,25 @@
 package com.nokhyun.samplestructure.ui.fragment
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import androidx.transition.ChangeBounds
 import androidx.transition.Fade
 import androidx.transition.Transition
 import androidx.transition.TransitionManager
 import com.nokhyun.samplestructure.R
 import com.nokhyun.samplestructure.databinding.FragmentTransitionBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TransitionFragment : BaseFragment<FragmentTransitionBinding>() {
+    private lateinit var frontAnim: AnimatorSet
+    private lateinit var backAnim: AnimatorSet
     override fun init() {
         binding.btnVisible.setOnClickListener {
             TransitionManager.beginDelayedTransition(binding.clFade, Fade(Fade.IN))
@@ -77,7 +84,7 @@ class TransitionFragment : BaseFragment<FragmentTransitionBinding>() {
                 }
             }
         }
-
+        flipAnim()
     }
 
     override fun navigator() {
@@ -89,5 +96,29 @@ class TransitionFragment : BaseFragment<FragmentTransitionBinding>() {
     private fun ViewGroup.beginDelayedTransition(transition: Transition, block: (ViewGroup) -> Unit) {
         TransitionManager.beginDelayedTransition(this, transition)
         block(this)
+    }
+
+    private fun flipAnim() {
+        var scale = resources.displayMetrics.density
+        val front = binding.ivFront
+        val back = binding.ivBack
+
+        front.cameraDistance = 8000 * scale
+        back.cameraDistance = 8000 * scale
+
+        frontAnim = AnimatorInflater.loadAnimator(requireContext(), R.animator.front_animator) as AnimatorSet
+        backAnim = AnimatorInflater.loadAnimator(requireContext(), R.animator.back_animator) as AnimatorSet
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            frontAnim.setTarget(front)
+            backAnim.setTarget(back)
+            frontAnim.start()
+            backAnim.start()
+            delay(5000)
+            backAnim.setTarget(front)
+            frontAnim.setTarget(back)
+            frontAnim.start()
+            backAnim.start()
+        }
     }
 }
