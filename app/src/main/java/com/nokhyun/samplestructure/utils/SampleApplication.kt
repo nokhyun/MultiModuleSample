692c50bea8f9473710901909f8998a87af852bd2
nokhyun90 <nokhyun90@gmail.com>
2024-05-20 23:15:10 +0900

    Added:Koin

--- app/src/main/java/com/nokhyun/samplestructure/utils/SampleApplication.kt
+++ app/src/main/java/com/nokhyun/samplestructure/utils/SampleApplication.kt
@@ -5,11 +5,22 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.dsl.module
 
 @HiltAndroidApp
 class SampleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            androidContext(this@SampleApplication)
+            androidLogger()
+            modules(firstModule)
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel("location", "Location", NotificationManager.IMPORTANCE_HIGH)
@@ -18,4 +29,9 @@ class SampleApplication : Application() {
             notificationManager.createNotificationChannel(channel)
         }
     }
+}
+
+// TODO Koin -> Hilt Migration 테스트 샘플용
+val firstModule = module {
+    // TODO
 }
