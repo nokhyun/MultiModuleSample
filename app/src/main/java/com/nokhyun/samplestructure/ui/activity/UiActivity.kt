0b658e272bfa7e37f8f06a105a201a51aee73f5e
nokhyun90 <nokhyun90@gmail.com>
2024-06-13 00:01:45 +0900

    오차

--- app/src/main/java/com/nokhyun/samplestructure/ui/activity/UiActivity.kt
+++ app/src/main/java/com/nokhyun/samplestructure/ui/activity/UiActivity.kt
@@ -2,6 +2,7 @@ package com.nokhyun.samplestructure.ui.activity
 
 //import com.nokhyun.samplestructure.ui.common.IndentLeadingMarginSpan
 import android.content.Context
+import android.graphics.Paint
 import android.os.Bundle
 import android.text.Layout
 import android.text.SpannableStringBuilder
@@ -69,7 +70,7 @@ class UiActivity : AppCompatActivity() {
         val textSize = 16f
         val typeface = resources.getFont(com.google.android.exoplayer2.ui.R.font.roboto_medium_numbers)
 
-        // Chip 생성 및 설정
+// Chip 생성 및 설정
         val chip = Chip(this).apply {
             this.textSize = textSize
             this.typeface = typeface
@@ -78,32 +79,47 @@ class UiActivity : AppCompatActivity() {
             // 예: setPadding(16, 16, 16, 16) // 패딩 값 설정
         }
 
-        // TextPaint를 사용하여 폰트 및 텍스트 크기 설정
+// TextPaint를 사용하여 폰트 및 텍스트 크기 설정
         val paint = TextPaint().apply {
             this.textSize = textSize
             this.typeface = typeface
         }
 
-        // TextPaint.measureText()를 호출할 때 Chip의 패딩 값을 고려하여 텍스트의 폭 측정
-        Timber.e("chip.chipStartPadding: ${chip.chipStartPadding} :: chip.paddingStart: ${chip.paddingStart}")
-        val textWidth = paint.measureText(txt) + chip.chipStartPadding + chip.chipEndPadding + chip.paddingStart + chip.paddingEnd
+// 각 문자의 너비를 측정하여 전체 텍스트의 폭 계산
+        val textWidth = measureTextWidth(paint, txt)
 
         val dummyViewGroup = LinearLayout(this).apply {
             layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
         }
 
-        // Chip을 LinearLayout에 추가
+// Chip을 LinearLayout에 추가
         dummyViewGroup.addView(chip)
 
-        // LinearLayout을 루트 뷰에 추가
+// LinearLayout을 루트 뷰에 추가
         (binding.root as ViewGroup).addView(dummyViewGroup)
 
-        // Chip의 너비를 측정하여 로그로 출력
+// Chip의 너비를 측정하여 로그로 출력
         chip.post {
             val chipWidth = chip.width
-            Timber.e("chipWidth: $chipWidth")
-            Timber.e("paint: $textWidth")
+            Timber.e("chipWidth: $chipWidth :: measureTextWidth: ${paint.measureText(txt)}")
+
+            // 오차 계산
+            val error = Math.abs(chipWidth - textWidth)
+            Timber.e("Error: $error")
+
+            Timber.e("after chipWidth: $chipWidth :: measureTextWidth: ${paint.measureText(txt) + error}")
         }
+
+    }
+
+    private fun measureTextWidth(paint: Paint, text: String): Float {
+        var width = 0f
+        val widths = FloatArray(text.length)
+        paint.getTextWidths(text, widths)
+        for (w in widths) {
+            width += w
+        }
+        return width
     }
 
     private fun dpToPx(context: Context, dp: Float): Float {
